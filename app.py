e91c4f0bd7057a5224f5bddf2ab21c8e654e00b2
Neeraj-programmer7 <neerajthakur15016@gmail.com>
2026-03-02 18:14:42 +0530

    Added gunicorn for production

--- app.py
+++ app.py
@@ -492,7 +492,8 @@ def delete_subject(id):
 
     return redirect("/subjects")
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
